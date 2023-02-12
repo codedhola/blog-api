733ef4995017f6af7866effd49e462380355c05a
codedhola <codedhola2000@gmail.com>
2023-02-12 18:59:09 +0100

    fix🔧: fix coalesce error in DB query

--- src/database/queries/blogQueries.js
+++ src/database/queries/blogQueries.js
@@ -7,7 +7,7 @@ const query = {
     WHERE blog_id = $1
     ;`,
     createBlog: `INSERT INTO blog(title, description, body, slug, author_id) VALUES($1, $2, $3, $4, $5) RETURNING *;`,
-    editBlog:`UPDATE blog SET title = COALESCE($1, title), body = $COALESCE($2, body), state = $COALESCE($3, state) WHERE id = S4 RETURNING *`,
+    editBlog:`UPDATE blog SET title = COALESCE($1, title), body = COALESCE($2, body), state = COALESCE($3, state) WHERE id = S4 RETURNING *`,
     deleteBlog: `DELETE FROM blog WHERE blog_id = $1`
 }
 
